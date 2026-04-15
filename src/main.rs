74a411d4c6fad0f2ccacb98a2698f5ae4b47de66
Bongo <hashir447079@gmail.com>
2026-04-15 11:17:20 +0500

    gonna switch to vala

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
-mod commands;
-mod config;
-mod logs;
-
-use clap::{Parser, Subcommand};
-
-#[derive(Parser)]
-#[command(name = "mc-manager")]
-#[command(about = "Minecraft Server Manager CLI", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Server {
-        #[arg(value_enum)]
-        action: commands::server::ServerAction,
-    },
-    Backup {
-        #[arg(value_enum)]
-        action: commands::backup::BackupAction,
-    },
-    Logs {
-        #[arg(short, long)]
-        tail: bool,
-    },
-}
+mod app;
 
 fn main() {
-    let cli = Cli::parse();
+    // Don't call gtk::init manually - let adw handle it
+    // Just set the environment variable before creating the app
+    std::env::set_var("GTK_CSD", "0");
 
-    match cli.command {
-        Commands::Server { action } => commands::server::handle_server(action),
-        Commands::Backup { action } => commands::backup::handle_backup(action),
-        Commands::Logs { tail } => logs::show_logs(tail),
-    }
+    app::run();
 }
